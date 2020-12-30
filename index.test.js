3ba953184ff50575f475293d2ff2c2385b4b45c5
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-30 09:20:06 -0600

    feat: forEach() test.

---
+++ index.test.js
@@ -0,0 +1,13 @@
+const { forEach, map } = require('./index');
+
+let sum = 0;
+forEach([1,2,3], (value) => {
+    sum = sum + value;
+})
+
+if (sum !== 6) {
+    throw new Error('Expected summing array to equal 6');
+}
+
+
+
