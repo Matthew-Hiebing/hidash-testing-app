d7200018db2d598362cc969881e99dbe35fcfd62
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-30 15:15:40 -0600

    refactor: Use assert.strictEqual instead of long if statements for error testing.  Use assert module.

--- index.test.js
+++ index.test.js
@@ -1,3 +1,4 @@
+const assert = require('assert')
 const { forEach, map } = require('./index');
 
 const test = (desc, fn) => {
@@ -15,6 +16,8 @@ test('The forEach function', () => {
         sum = sum + value;
     })
 
+    assert.strictEqual(sum, 6, 'Expected forEach to sum the array');
+
     if (sum !== 6) {
         throw new Error('Expected summing array to equal 6');
     }
@@ -26,15 +29,9 @@ test('The map function', () => {
         return value * 2;
     });
 
-    if (result[0] !== 2) {
-        throw new Error(`Expected to find 2, but found ${result[0]}`)
-    }
-    if (result[1] !== 4) {
-        throw new Error(`Expected to find 4, but found ${result[0]}`)
-    }
-    if (result[2] !== 6) {
-        throw new Error(`Expected to find 6, but found ${result[0]}`)
-    }
+    assert.strictEqual(result[0], 2);
+    assert.strictEqual(result[1], 4);
+    assert.strictEqual(result[2], 6);
     // result === [2,4,6]
 });
 
